189e704ad7337466ce28e8b089d5b63c9d3b12d7
KennyAkmal <snowzly404@gmail.com>
2024-06-12 19:58:26 +0700

    validate login function

--- Documents/NetBeansProjects/HoloCommerce/src/java/logic/validate.java
+++ Documents/NetBeansProjects/HoloCommerce/src/java/logic/validate.java
@@ -1,5 +1,7 @@
 package logic;
 
+import jdbc.dbcon;
+import java.sql.*;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -15,18 +17,46 @@ public class validate extends HttpServlet {
         try {
             String name = request.getParameter("user");
             String password = request.getParameter("pass");
+            Connection conn = null;
+            PreparedStatement pstmt = null;
+            ResultSet rs = null;
 
-            if (password.equals("kenny") && name.equals("kenny")) {
-                // Menyimpan nama ke dalam sesi
-                HttpSession session = request.getSession();
-                session.setAttribute("user", name);
+            try {
+                // Connect to the database
+                dbcon konek = new dbcon();
+                conn = konek.bukaKoneksi();
+                String sql = "SELECT * FROM users WHERE nama = ? AND pass = ?";
+                pstmt = conn.prepareStatement(sql);
+                pstmt.setString(1, name);
+                pstmt.setString(2, password);
+                rs = pstmt.executeQuery();
 
-                RequestDispatcher rd = request.getRequestDispatcher("homepage.jsp");
-                rd.forward(request, response);
-            } else {
-                request.setAttribute("errorMessage", "<font color='red'><b>Invalid username or password.</b></font><br><br>");
+                if (rs.next()) {
+                    // User found, set session and redirect to homepage
+                    HttpSession session = request.getSession();
+                    session.setAttribute("user", name);
+
+                    RequestDispatcher rd = request.getRequestDispatcher("homepage.jsp");
+                    rd.forward(request, response);
+                } else {
+                    // User not found, send error message
+                    request.setAttribute("errorMessage", "<font color='red'><b>Invalid username or password.</b></font><br><br>");
+                    RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+                    rd.include(request, response);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                request.setAttribute("errorMessage", "<font color='red'><b>Error: " + e.getMessage() + "</b></font><br><br>");
                 RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
                 rd.include(request, response);
+            } finally {
+                try {
+                    if (rs != null) rs.close();
+                    if (pstmt != null) pstmt.close();
+                    if (conn != null) conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         } finally {
             out.close();
